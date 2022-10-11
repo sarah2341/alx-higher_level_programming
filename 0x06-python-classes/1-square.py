86f1b500a36cd649ea0227fcc95ae04c47048891
Chetachi <maduchetat231@gmail.com>
2022-10-11 13:58:29 -0700

    Task

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,12 +1,10 @@
 #!/usr/bin/python3
 """Square class definition"""
 
-class Square:
-    """Represents a square
 
-    Attributes:
-        __size (int): size of a side of the square
-    """
+class Square:
+    """Represents a square"""
+
     def __init__(self, size):
         """Initializes a square
         Args:
